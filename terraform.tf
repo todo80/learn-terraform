ff28fa4773ccd2a358d787108c2427f1bfc03ff8
todo80 <trunghh@gmail.com>
2024-04-30 21:42:28 +0300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,14 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "ktowey"
 
     workspaces {
       name = "learn-terraform"
     }
   }
-
+*/
+  
   required_providers {
     aws = {
       source  = "hashicorp/aws"
